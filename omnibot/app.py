b1dacbbb8cfa2677a45f69290f8f1feb77955543
Ryan Lane <rlane@lyft.com>
2019-11-21 17:21:42 -0800

    Remove unnecessary files

--- omnibot/app.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-from flask import Flask
-
-from omnibot import settings
-
-app = Flask(__name__)
-app.config.from_object(settings)
--- omnibot/callbacks/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Callbacks internal to omnibot. Please only add handlers here that are for
-generic functionality of omnibot. This is not a location for real bots.
-"""
