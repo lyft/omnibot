b6b1701c0a4663f522cbdccfc72beb69d3ad5788
ryan-lane <rlane32@gmail.com>
2019-11-22 04:11:39 +0000

    Deploying to gh-pages from master ffeb7abd540c29a6c968d8ffddbd6697f914f3a8

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0-6f5c81',
+    VERSION: '1.0.0-ffeb7a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
