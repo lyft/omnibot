567cb188c86099835cd79eb759298d709fa23311
Kevin Xiao <kxiao@lyft.com>
2021-05-18 16:03:15 -0400

    Add assert called once with check

--- tests/integration/routes/test_get_channel.py
+++ tests/integration/routes/test_get_channel.py
@@ -2,6 +2,8 @@ from flask import Response  # noqa: F401
 from pytest_mock import MockerFixture
 from werkzeug.test import Client
 
+from tests.integration.routes import get_test_bot
+
 
 def test_get_channel_by_name(internal_client: Client, mocker: MockerFixture):
     get_channel_by_name = mocker.patch("omnibot.services.slack.get_channel_by_name")
@@ -12,6 +14,7 @@ def test_get_channel_by_name(internal_client: Client, mocker: MockerFixture):
     )
     assert resp.status_code == 200
     assert resp.json == mock_channel_value
+    get_channel_by_name.assert_called_once_with(get_test_bot(), "test-channel")
 
 
 def test_get_channel_by_name_invalid(internal_client: Client, mocker: MockerFixture):
@@ -22,3 +25,4 @@ def test_get_channel_by_name_invalid(internal_client: Client, mocker: MockerFixt
     )
     assert resp.status_code == 404
     assert resp.json["error"] == "provided channel_name was not found."
+    get_channel_by_name.assert_called_once_with(get_test_bot(), "test-channel")
