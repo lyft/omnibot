963b509a76400b5250bb83d7822cc71ca4e20483
haoxue1108 <57418867+haoxue1108@users.noreply.github.com>
2021-08-06 18:09:09 +0000

    Deploying to gh-pages from master @ 8e9cc270d8f415806a6016bc2a64d6760f854e77 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.3-0e7cf1',
+    VERSION: '1.0.3-8e9cc2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
