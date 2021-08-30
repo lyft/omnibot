5305f9bde232e593869cff55272ccd12581318f5
Hao Xue <57418867+haoxue1108@users.noreply.github.com>
2021-08-30 13:30:03 -0700

    Revert "fix watchers on elasticache clustered mode (#42)" (#43)
    This reverts commit 16e4bc265b299e36abb95260e6facb91728eb956.

--- omnibot/watcher.py
+++ omnibot/watcher.py
@@ -60,10 +60,7 @@ def watch_users():
         statsd = stats.get_statsd_client()
         with redis_lock.Lock(
                 redis_client,
-                # we prepend an elasticache {hash_key}
-                # to this and other redis locks
-                # so that they can function in clustered mode
-                '{watch_users}:watch_users',
+                'watch_users',
                 expire=LOCK_EXPIRATION,
                 auto_renewal=True):
             with statsd.timer('watch.users'):
@@ -98,7 +95,7 @@ def watch_conversations():
         statsd = stats.get_statsd_client()
         with redis_lock.Lock(
                 redis_client,
-                '{watch_conversation}:watch_conversation',
+                'watch_conversation',
                 expire=LOCK_EXPIRATION,
                 auto_renewal=True):
             with statsd.timer('watch.conversation'):
@@ -133,7 +130,7 @@ def watch_emoji():
         statsd = stats.get_statsd_client()
         with redis_lock.Lock(
                 redis_client,
-                '{watch_emoji}:watch_emoji',
+                'watch_emoji',
                 expire=LOCK_EXPIRATION,
                 auto_renewal=True):
             with statsd.timer('watch.emoji'):
