6ad901183d96c7dbe27735eeb4859f48b44617ed
kvnxiao <xlzqwerty1@gmail.com>
2022-08-02 20:55:17 +0000

    Deploying to gh-pages from master @ cb1b12ca19782b231ec7f37eab60903540dc6191 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.3-8e9000',
+    VERSION: '1.0.3-cb1b12',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
